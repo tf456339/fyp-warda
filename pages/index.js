86bf0f04527e09abaff32ed8b654dba39643859d
w3gym <www.w3gym.com@gmail.com>
2021-07-19 06:00:05 +0500

    home page fixed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 const Home = ()=>{
   return(
-   <iframe  frameBorder="0" style={{ height: '100%' }} height="100%" width="100%" src="http://localhost:3000/home.html" ></iframe>
+   <iframe  frameBorder="0" style={{ height: '100%' }} height="100%" width="100%" src="./home.html" ></iframe>
   )
 }
 
